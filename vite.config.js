128d80211a36d4b88d91ab948cf6d9cecc254018
ZOBLI Elyess <zobli@et.intechinfo.fr>
2024-07-01 20:56:13 +0200

    mise en place des styles

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
+
 
 export default defineConfig ({
     root: "src",
     build: {
         outDir : "../dist",
+        rollupOptions:{
+            input: {
+                index: resolve(__dirname, 'src/index.html'),
+                form: resolve(__dirname, 'src/form/form.html'),
+              },
+        }
     },
 });
