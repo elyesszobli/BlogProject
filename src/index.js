dda2f08fe8b15992f14012724ab984442fe05ad3
elyesszobli <elyesszobli@gmail.com>
2024-08-08 17:15:46 +0200

    mise a jour

--- src/index.js
+++ src/index.js
@@ -1,40 +1,45 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
+import "./assets/javascripts/topbar";
 
 const articleContainerElement = document.querySelector(".articles-container");
 
-const creatArticles = (articles) => {
-  const articlesDOOM = articles.map((article) => {
+const createArticles = articles => {
+  const articlesDOM = articles.map(article => {
     const articleDOM = document.createElement("div");
     articleDOM.classList.add("article");
     articleDOM.innerHTML = `
-        
-            <img src="${article.img}" alt="profile">
-            <h2>${article.title}</h2>
-            <p class="article-author">${article.author} - ${article.category}</p>
-            <p class="article-content">${article.content}</p>
-            <div class="article-action">
-              <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
-            </div>
-          
-        `;
+<img
+  src="${article.img}"
+  alt="profile"
+/>
+<h2>${article.title}</h2>
+<p class="article-author">${article.author} - ${article.category}</p>
+<p class="article-content">
+  ${article.content}
+</p>
+<div class="article-actions">
+  <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
+</div>
+`;
     return articleDOM;
   });
   articleContainerElement.innerHTML = "";
-  articleContainerElement.append(...articlesDOOM);
+  articleContainerElement.append(...articlesDOM);
   const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
-  deleteButtons.forEach((button) => {
-    button.addEventListener("click", async (event) => {
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", async event => {
       try {
         const target = event.target;
         const articleId = target.dataset.id;
         const response = await fetch(
           `https://restapi.fr/api/article/${articleId}`,
           {
-            method: "DELETE",
+            method: "DELETE"
           }
         );
         const body = await response.json();
+        console.log(body);
         fetchArticle();
       } catch (e) {
         console.log("e : ", e);
@@ -46,11 +51,14 @@ const creatArticles = (articles) => {
 const fetchArticle = async () => {
   try {
     const response = await fetch("https://restapi.fr/api/article");
-    const articles = await response.json();
-    console.log(articles);
-    creatArticles(articles);
+    let articles = await response.json();
+    if (!Array.isArray(articles)) {
+      articles = [articles];
+    }
+    createArticles(articles);
   } catch (e) {
     console.log("e : ", e);
   }
 };
+
 fetchArticle();
